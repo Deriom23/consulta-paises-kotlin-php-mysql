1bb507b25b1fd22c33a50179e63f004ab865c8e1
Deriom23 <eliasrivasdavila@gmail.com>
2026-09-05 19:11:02 -0500

    Permitir configuracion de backend desde ejemplo

--- backend/config.php
+++ backend/config.php
@@ -1,3 +1,4 @@
 <?php
 
-return require __DIR__ . '/config.local.php';
+$localConfig = __DIR__ . '/config.local.php';
+return require file_exists($localConfig) ? $localConfig : __DIR__ . '/config.example.php';
